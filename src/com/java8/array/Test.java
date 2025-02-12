26fece91ace620f55d73f5fff3dacf2f2003dba4
RamabrahmamFS <togitiram26@gmail.com>
2025-02-11 19:37:38 -0500

    Java 8 Features.

---
+++ src/com/java8/array/Test.java
@@ -0,0 +1,15 @@
+package com.java8.array;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Test {
+
+    public static void main(String[] args) {
+        List<Integer> listOfIntegers = Arrays.asList(45, 12, 56, 15, 24, 75, 31, 89);
+
+        Integer secondLargestNumber = listOfIntegers.stream().sorted(Comparator.reverseOrder()).skip(2).findFirst().get();
+        System.out.println(secondLargestNumber);
+    }
+}
